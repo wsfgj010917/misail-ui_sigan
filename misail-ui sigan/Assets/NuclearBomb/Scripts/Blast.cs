2f30967f648b1c29c7be29ed1df794300ecbd0ec
KOMPUTRONIK\Czort <pbulak@gmail.com>
2017-09-02 18:52:34 +0200

    Zabawy z UI, WYBUCH!

---
+++ misail-ui sigan/Assets/NuclearBomb/Scripts/Blast.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Experimental: needs a lot of improvement!
+/// </summary>
+
+public class Blast : MonoBehaviour {
+	public GameObject[] debris;
+	public int debrisCount;
+	public bool generateDebris;
+	
+	void OnTriggerEnter (Collider col) {
+		if (hasMeshRenderer (col.gameObject) && !col.isTags (new string[2] { "Nuke", "Debris" }))
+			StartCoroutine (Disintegrate (col.gameObject));
+	}
+	
+	bool hasMeshRenderer (GameObject obj) {
+		return obj.GetComponent <Renderer> () != null || obj.GetComponentInChildren <Renderer> () != null;
+	}
+	
+	Color distanceBasedGrayShade (float distance) {
+		float s = Mathf.Clamp (Mathf.Pow (distance / 200, 2), 0, 1);
+		return new Color (s, s, s);
+	}
+	
+	IEnumerator Disintegrate (GameObject go) {
+		float time = 0.225f;
+		Color initialColor = distanceBasedGrayShade (Vector3.Distance (go.transform.position, transform.position));
+		//Make the object transparent, so we can fade it:
+		if (go.GetComponent <Renderer> ()) {
+			go.GetComponent <Renderer> ().material.SetFloat ("_Mode", 2);
+		} else {
+			go.GetComponentInChildren <Renderer> ().material.SetFloat ("_Mode", 2);
+		}
+		for (float t = 0; t < 1.0f; t += Time.deltaTime / time) {
+			//Makes the object black
+			if (go.GetComponent <Renderer> ()) {
+				go.GetComponent <Renderer> ().material.color = Color.Lerp (
+					Color.white,
+					initialColor,
+					t
+				);
+			} else {
+				go.GetComponentInChildren <Renderer> ().material.color = Color.Lerp (
+					Color.white,
+					initialColor,
+					t
+				);
+			}
+			yield return 0;
+		}
+		Random.seed = (int)System.DateTime.Now.Ticks;
+		if (Random.value > .05f || Vector3.Distance (go.transform.position, transform.position) < 100) {
+			time = 0.025f;
+			for (float t = 0; t < 1.0f; t += Time.deltaTime / time) {
+				//Fades the object away
+				if (go.GetComponent <Renderer> ()) {
+					go.GetComponent <Renderer> ().material.color = Color.Lerp (
+						Color.black,
+						new Color32 (0, 0, 0, 0),
+						t
+					);
+				} else {
+					go.GetComponentInChildren <Renderer> ().material.color = Color.Lerp (
+						Color.black,
+						new Color32 (0, 0, 0, 0),
+						t
+					);
+				}
+				yield return 0;
+			}
+			
+			if (debris.Length > 0 && generateDebris) {
+				//Generate a bunch of meshes to use as debris
+				for (int i = 0; i < (int)(debrisCount * go.transform.localScale.magnitude); i++) {
+					GameObject g = Instantiate (debris[Random.Range (0, debris.Length - 1)], go.transform.position + Random.insideUnitSphere * go.transform.localScale.x, Random.rotation) as GameObject;
+					g.SetActive (true);
+					g.hideFlags = HideFlags.HideInHierarchy;
+					g.AddComponent <Rigidbody> ().mass = .1f;
+					g.GetComponent <Rigidbody> ().AddExplosionForce (80, transform.position, 500);
+					g.AddComponent <AutoDestroyInXSeconds> ().t = Random.Range (3, 5);
+				}
+			} else if (generateDebris) Debug.LogWarning ("No debris meshes were added, skipping");
+			
+			Destroy (go);
+		} else
+			if (go.GetComponent <Renderer> ()) {
+				go.GetComponent <Renderer> ().material.SetFloat ("_Mode", 0);
+			} else {
+				go.GetComponentInChildren <Renderer> ().material.SetFloat ("_Mode", 0);
+			}
+	}
+}
+
+public class AutoDestroyInXSeconds : MonoBehaviour {
+	public float t;
+	
+	IEnumerator Start () {
+		yield return new WaitForSeconds (t);
+		Destroy (gameObject);
+	}
+}
---
+++ misail-ui sigan/Assets/NuclearBomb/Scripts/Nuke.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Nuke : MonoBehaviour {
+    public GameObject Head;
+    public ParticleSystem Body;
+    public float Target;
+    public GameObject blast;
+    public LensFlare _light;
+    public GameObject bomb;
+    public GameObject[] particles;
+    public GameObject crater;
+    public Light m_light;
+    public bool detonateByE;
+    
+    [Range (1, 10)]
+    public float time;
+    private float m_height;
+    private bool detonated;
+    private Renderer m_blast;
+    private float m_speed = 1;
+
+	// Use this for initialization
+	void Start () {
+        //Set speed base on body's particle speed and the height based on Target's Y postion
+        this.m_height = Target;
+        blast.transform.localScale = Vector3.zero;
+        crater.GetComponent <Renderer> ().material.renderQueue = 2000;
+        blast.GetComponent <Renderer> ().material.renderQueue = 2000;
+        m_blast = blast.GetComponent <Renderer> ();
+    }
+	
+	public void Detonate () {
+		StartCoroutine (flare ());
+		foreach (GameObject g in particles) g.SetActive (true);
+		detonated = true;
+		Destroy (bomb);
+		StartCoroutine (showCrater ());
+		StartCoroutine (fadeLight ());
+	}
+	
+	IEnumerator showCrater () {
+		yield return new WaitForSeconds (2);
+		crater.SetActive (true);
+	}
+	
+	IEnumerator fadeLight () {
+		float init = m_light.intensity;
+		float time = 5;
+		for (float t = 0; t < 1; t += Time.deltaTime / time) {
+			m_light.intensity = Mathf.Lerp (
+				init,
+				0,
+				t
+			);
+			yield return 0;
+		}
+		Destroy (GetComponent<Light>());
+	}
+	
+	IEnumerator flare () {
+		for (float t = 0; t < 1; t += Time.deltaTime * 5) {
+			_light.brightness = Mathf.Lerp (
+				_light.brightness,
+				5,
+				t
+			);
+			yield return 0;
+		}
+		for (float t = 0; t < 1; t += Time.deltaTime / 250) {
+			_light.brightness = Mathf.Lerp (
+				_light.brightness,
+				0,
+				t
+			);
+			_light.color = Color.Lerp (
+				_light.color,
+				Color.clear,
+				t
+			);
+			yield return 0;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (detonated) {
+			Head.transform.localPosition = Vector3.Lerp (
+				Head.transform.localPosition,
+				Vector3.zero.SetZ (m_height),
+				Time.deltaTime / time
+			);
+			if (blast != null)
+				if(blast.transform.localScale.x > 5f) Destroy (blast);
+				else {
+					blast.transform.localScale += (Vector3.one / 100) * m_speed;
+					SetAlpha (Mathf.Lerp (m_blast.material.color.a, 0, Time.deltaTime / 3));
+					m_speed = Mathf.Clamp (m_speed - .001f, 0, 1);
+				}
+		} else if (Input.GetKeyDown (KeyCode.E) && detonateByE) Detonate ();
+	}
+	
+	void SetAlpha (float a) {
+		Color init = m_blast.material.color;
+		init.a = a;
+		m_blast.material.color = init;
+	}
+}
+
+public static class extensions {
+	public static Vector3 SetX (this Vector3 vec, float x) { vec.x = x; return vec; }
+	public static Vector3 SetY (this Vector3 vec, float y) { vec.y = y; return vec; }
+	public static Vector3 SetZ (this Vector3 vec, float z) { vec.z = z; return vec; }
+	public static void copyFrom (this Transform t, Transform other) {
+		t.position = other.position;
+		t.rotation = other.rotation;
+		t.localScale = other.localScale;
+	}
+	public static bool isTags (this Collider col, string[] tags) {
+		foreach (string tag in tags) if (tag == col.tag) return true;
+		return false;
+	}
+}
